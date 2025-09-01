18aa1fc74d367686415685be7f74a28587d07f15
Mohamed-cultureM13 <60194993+Mohamed-cultureM13@users.noreply.github.com>
2025-09-01 17:00:31 +0300

    Update urls.py

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -7,9 +7,4 @@ urlpatterns = [
     path('whatsapp-webhook/', views.whatsAppWebhook, name='whatsapp_webhook'),
 ]
 
-#webhook url
-#https://fidelio36.pythonanywhere.com/whatsapp-webhook/  #for python-anywhere
-
-#token - kwa ajili ya testing, nitatengeneza nyingine wakati wa production kabla ya kudeploy.
-# 6ab1d155-e097-4dde-bf4d-8093efb4d503
 
