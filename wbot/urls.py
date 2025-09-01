0f5f719eb996cefe3262c97831a5d1f03953703e
Mohamed-cultureM13 <60194993+Mohamed-cultureM13@users.noreply.github.com>
2025-09-01 16:56:53 +0300

    Update urls.py

--- wbot/urls.py
+++ wbot/urls.py
@@ -2,11 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('chat/', include('chatbot.urls')),
+    path('', RedirectView.as_view(url='/chat/', permanent=False)),
 ]
 
 # if settings.DEBUG:
 #     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
